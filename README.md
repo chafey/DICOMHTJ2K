4cc32a9abefe65df0de99fbcc34c543fe804d107
Pantelis Georgiadis <PantelisGeorgiadis@Gmail.com>
2023-11-07 15:51:32 +0200

    Added support for multiframe and encapsulated syntaxes

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # DICOMHTJ2K
 DICOM HTJ2K tools
 
-
 ## Transcode DICOM P10 file to DICOM P10 HTJ2K
 
 ### Limitations
-
-* Does not support multi-frame (does not check - will only transcode first framt)
-* Does not support compressed input images (does not check - will HTJ2K encode the compressed image frame)
-* Does not set the photometric interpretation properly for color images (leaves PhotoMetric interp as it is)
+* Does not set the photometric interpretation properly for color images (leaves photometric interpretation as it is)
 * Uses the JPEG2000 Lossless only transfer syntax (this will be updated to the official one once it is decided)
 
 ### Setup
--- index.js
+++ index.js
@@ -1,80 +1,210 @@
-let dcmjs = require('dcmjs')
-let fs = require('fs')
-let openjphjs = require('./extern/openjphjs/dist/openjphjs.js')
+const fs = require('fs');
+const dcmjs = require('dcmjs');
+const openjphjs = require('./extern/openjphjs/dist/openjphjs.js');
+
+const dcmjsImaging = require('dcmjs-imaging');
+// Mimics a class not exported by dcmjs-imaging
+const pixelShim = (
+  width,
+  height,
+  bitsAllocated,
+  bitsStored,
+  samplesPerPixel,
+  pixelRepresentation,
+  planarConfiguration,
+  photometricInterpretation
+) => ({
+  getWidth: () => width,
+  getHeight: () => height,
+  getBitsAllocated: () => bitsAllocated,
+  getBitsStored: () => bitsStored,
+  getSamplesPerPixel: () => samplesPerPixel,
+  getPixelRepresentation: () => pixelRepresentation,
+  getPlanarConfiguration: () => planarConfiguration,
+  getPhotometricInterpretation: () => photometricInterpretation,
+});
 
 const getInputFilePath = () => {
-    if (process.argv.length < 3) {
-        console.error('Please provide path to input DICOM P10 file');
-        process.exit(1);
-    }
+  if (process.argv.length < 3) {
+    console.error('Please provide path to input DICOM P10 file');
+    process.exit(1);
+  }
 
-    const filePath = process.argv[2]
-    return filePath
-}
+  const filePath = process.argv[2];
+  return filePath;
+};
 
 const getOutputFilePath = () => {
-    if (process.argv.length < 4) {
-        console.error('Please provide path to output DICOM P10 file');
-        process.exit(1);
+  if (process.argv.length < 4) {
+    console.error('Please provide path to output DICOM P10 file');
+    process.exit(1);
+  }
+
+  const filePath = process.argv[3];
+  return filePath;
+};
+
+const getFrameBuffer = (dataset, transferSyntaxUid, frame) => {
+  if (frame < 0 || frame >= dataset.NumberOfFrames) {
+    throw new Error(`Requested frame is out of range [${frame}]`);
+  }
+  if (!dataset.PixelData) {
+    throw new Error('Could not extract pixel data');
+  }
+  if (!dataset.Columns || !dataset.Rows) {
+    throw new Error(
+      `Width/height has an invalid value [w: ${dataset.Columns}, h: ${dataset.Rows}]`
+    );
+  }
+  if (!dataset.BitsAllocated || !dataset.BitsStored) {
+    throw new Error(
+      `Bits allocated/stored has an invalid value [allocated: ${dataset.BitsAllocated}, stored: ${dataset.BitsStored}]`
+    );
+  }
+  if (!dataset.PhotometricInterpretation) {
+    throw new Error(
+      `Photometric interpretation has an invalid value [${dataset.PhotometricInterpretation}]`
+    );
+  }
+
+  const pixelBuffers = dataset.PixelData;
+  if (
+    transferSyntaxUid === '1.2.840.10008.1.2' || // ImplicitVRLittleEndian
+    transferSyntaxUid === '1.2.840.10008.1.2.1' || // ExplicitVRLittleEndian
+    transferSyntaxUid === '1.2.840.10008.1.2.1.99' // DeflatedExplicitVRLittleEndian
+  ) {
+    const frameSize =
+      ((dataset.Columns * dataset.Rows * dataset.BitsAllocated) / 8) * dataset.SamplesPerPixel;
+    const frameOffset = frameSize * frame;
+    const pixelBuffer = new Uint8Array(
+      Array.isArray(pixelBuffers) ? pixelBuffers.find((o) => o) : pixelBuffers
+    );
+    const framePixelBuffer = pixelBuffer.slice(frameOffset, frameOffset + frameSize);
+
+    return framePixelBuffer;
+  } else {
+    const frameFragmentsData = new Uint8Array(pixelBuffers[frame]);
+    const pixel = pixelShim(
+      dataset.Columns,
+      dataset.Rows,
+      dataset.BitsAllocated,
+      dataset.BitsStored,
+      dataset.SamplesPerPixel,
+      dataset.PixelRepresentation,
+      dataset.PlanarConfiguration,
+      dataset.PhotometricInterpretation
+    );
+
+    if (transferSyntaxUid === '1.2.840.10008.1.2.5') {
+      // RleLossless
+      return dcmjsImaging.NativePixelDecoder.decodeRle(pixel, frameFragmentsData);
+    } else if (
+      // JpegBaselineProcess1, JpegBaselineProcess2_4
+      transferSyntaxUid === '1.2.840.10008.1.2.4.50' ||
+      transferSyntaxUid === '1.2.840.10008.1.2.4.51'
+    ) {
+      return dcmjsImaging.NativePixelDecoder.decodeJpeg(pixel, frameFragmentsData, {
+        convertColorspaceToRgb: true,
+      });
+    } else if (
+      // JpegLosslessProcess14, JpegLosslessProcess14V1
+      transferSyntaxUid === '1.2.840.10008.1.2.4.57' ||
+      transferSyntaxUid === '1.2.840.10008.1.2.4.70'
+    ) {
+      return dcmjsImaging.NativePixelDecoder.decodeJpeg(pixel, frameFragmentsData);
+    } else if (
+      // JpegLsLossless, JpegLsLossy
+      transferSyntaxUid === '1.2.840.10008.1.2.4.80' ||
+      transferSyntaxUid === '1.2.840.10008.1.2.4.81'
+    ) {
+      return dcmjsImaging.NativePixelDecoder.decodeJpegLs(pixel, frameFragmentsData);
+    } else if (
+      // Jpeg2000Lossless, Jpeg2000Lossy
+      transferSyntaxUid === '1.2.840.10008.1.2.4.90' ||
+      transferSyntaxUid === '1.2.840.10008.1.2.4.91'
+    ) {
+      return dcmjsImaging.NativePixelDecoder.decodeJpeg2000(pixel, frameFragmentsData);
     }
 
-    const filePath = process.argv[3]
-    return filePath
-}
+    throw new Error(`Transfer syntax cannot be currently decoded [${transferSyntaxUid}]`);
+  }
+};
 
-openjphjs.onRuntimeInitialized = async _ => {
-    try {
-        // get input parameters
-        const inputFilePath = getInputFilePath()
-        const outputFilePath = getOutputFilePath()
+openjphjs.onRuntimeInitialized = async (_) => {
+  try {
+    // get input parameters
+    const inputFilePath = getInputFilePath();
+    const outputFilePath = getOutputFilePath();
 
-        //////////////////////////////////////////////////////////////////
-        // READ AND PARSE THE DICOM P10 FILE 
-        //////////////////////////////////////////////////////////////////
+    // initialize decoders
+    await dcmjsImaging.NativePixelDecoder.initializeAsync();
 
-        // Read the input DICOM P10 file
-        const b = fs.readFileSync(inputFilePath)
-        const dicomP10 = new Uint8Array(b.buffer, b.byteOffset, b.byteLength / Uint8Array.BYTES_PER_ELEMENT);
+    //////////////////////////////////////////////////////////////////
+    // READ AND PARSE THE DICOM P10 FILE
+    //////////////////////////////////////////////////////////////////
 
-        // Parse the DICOM P10 file and get the pixel data for the first frame
-        const dicomData = dcmjs.data.DicomMessage.readFile(dicomP10.buffer);
-        const meta = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.meta);
-        const dataset = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.dict);
-        const pixelData = new Uint8Array(dataset.PixelData[0])
+    // read the input DICOM P10 file
+    const b = fs.readFileSync(inputFilePath);
+    const dicomP10 = new Uint8Array(
+      b.buffer,
+      b.byteOffset,
+      b.byteLength / Uint8Array.BYTES_PER_ELEMENT
+    );
 
-        //////////////////////////////////////////////////////////////////
-        // ENCODE THE PIXEL DATA AS HTJ2k 
-        //////////////////////////////////////////////////////////////////
+    // parse the DICOM P10 file and get the pixel data for each frame
+    const dicomData = dcmjs.data.DicomMessage.readFile(dicomP10.buffer);
+    const meta = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.meta);
+    const transferSyntaxUid = meta.TransferSyntaxUID;
+    const dataset = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.dict);
+    const numberOfFrames = dataset.NumberOfFrames || 1;
 
-        // create the ImageFrame objet based on DICOM attributes of the input file
-        const imageFrame = {
-            width: dataset.Columns,
-            height: dataset.Rows,
-            bitsPerSample: dataset.BitsStored,
-            componentCount: dataset.SamplesPerPixel ?? 1,
-            isSigned: dataset.PixelRepresentation == 1,
-            isUsingColorTransform: dataset.SamplesPerPixel === 3
-        }
+    //////////////////////////////////////////////////////////////////
+    // ENCODE THE PIXEL DATA AS HTJ2k
+    //////////////////////////////////////////////////////////////////
+    
+    const encodedFrames = [];
+    for (let i = 0; i < numberOfFrames; i++) {
+      // get frame pixel data
+      const framePixelData = getFrameBuffer(dataset, transferSyntaxUid, i);
 
-        // encode the pixel data to htj2k
-        const encoder = new openjphjs.HTJ2KEncoder();
-        const decodedBytes = encoder.getDecodedBuffer(imageFrame);
-        decodedBytes.set(pixelData);
-        encoder.encode();
-        const encodedBytes = encoder.getEncodedBuffer();
-        console.log('  encoded length=', encodedBytes.length)
+      // create the ImageFrame objet based on DICOM attributes of the input file
+      const imageFrame = {
+        width: dataset.Columns,
+        height: dataset.Rows,
+        bitsPerSample: dataset.BitsStored,
+        componentCount: dataset.SamplesPerPixel ?? 1,
+        isSigned: dataset.PixelRepresentation == 1,
+        isUsingColorTransform: dataset.SamplesPerPixel === 3,
+      };
 
-        //////////////////////////////////////////////////////////////////
-        // WRITE OUT DICPOM P10 FILE WITH HTJ2K PIXEL DATA 
-        //////////////////////////////////////////////////////////////////
-        meta.TransferSyntaxUID = { Value: ['1.2.840.10008.1.2.4.90'] } // USE JPEG2000 for now...
-        dataset.PixelData[0] = encodedBytes.buffer.slice(0, encodedBytes.length)
-        dataset._meta = meta
-        const dicomP10New = dcmjs.data.datasetToBuffer(dataset)
-        fs.writeFileSync(outputFilePath, dicomP10New)
+      // encode the pixel data to htj2k
+      const encoder = new openjphjs.HTJ2KEncoder();
+      const decodedBytes = encoder.getDecodedBuffer(imageFrame);
+      decodedBytes.set(framePixelData);
+      encoder.encode();
+      const encodedBytes = encoder.getEncodedBuffer();
+
+      console.log(`  frame=${i}, encoded length=${encodedBytes.length}`);
+      encodedFrames.push(
+        encodedBytes.buffer.slice(
+          encodedBytes.byteOffset,
+          encodedBytes.byteLength + encodedBytes.byteOffset
+        )
+      );
     }
-    catch (ex) {
-        console.log('exception', ex)
-    }
-}
 
+    //////////////////////////////////////////////////////////////////
+    // WRITE OUT DICOM P10 FILE WITH HTJ2K PIXEL DATA
+    //////////////////////////////////////////////////////////////////
+    
+    meta.TransferSyntaxUID = { Value: ['1.2.840.10008.1.2.4.90'] }; // USE JPEG2000 for now...
+    dataset.PixelData = encodedFrames;
+    dataset._meta = meta;
+    const dicomP10New = Buffer.from(
+      dcmjs.data.datasetToDict(dataset).write({ fragmentMultiframe: false })
+    );
+    fs.writeFileSync(outputFilePath, dicomP10New);
+  } catch (ex) {
+    console.log('exception', ex);
+  }
+};
